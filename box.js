192ffcb608fc119bfb8394f996312e8ff9d8460b
arnav-234 <71269049+arnav-234@users.noreply.github.com>
2020-10-12 20:31:18 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class box{
+constructor(x,y,width,height)
+{
+    var options=
+{
+    restitution:0
+
+}
+this.body=Bodies.rectangle(x,y,width,height)
+World.add(world,this.body)
+}
+display(){
+var pos=this.body.position
+rectMode(CENTER)
+fill(255,0,0)
+rect(pos.x,pos.y,this.width,this.height)
+}
+}
